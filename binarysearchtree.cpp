77c7ac6e343cc3100e5315014009d5efbb77a912
nabaelhaq-gif <nabaelhaq@gmail.com>
2026-08-25 10:19:29 +0700

    menambah class & akses

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -20,3 +20,16 @@ public :
 
 };
 
+//membuat class BinaryTree
+class BinaryTree {
+
+//memberi akses modifier dan deklarasi object
+public :
+    Node* ROOT;
+
+    //membuat constructor dan memberi nilai kepada object root
+    BinaryTree() {
+        ROOT = nullptr;
+    }
+
+    

d4a300d5a640d4a67957f7b493c77769c14b4eec
nabaelhaq-gif <nabaelhaq@gmail.com>
2026-08-25 10:30:54 +0700

    menambah case 2

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -151,6 +151,10 @@ int main()
             x.insert(word);
             break;
         }
+        case '2': {
+            x.inorder(x.ROOT);
+            break;
+        }
         
         }
     }

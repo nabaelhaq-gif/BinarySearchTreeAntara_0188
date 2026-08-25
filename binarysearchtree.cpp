5d105a1d745845b4807f6f8792ef57297c9333a5
nabaelhaq-gif <nabaelhaq@gmail.com>
2026-08-25 10:18:29 +0700

    first commit

---
+++ binarysearchtree.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+//membuat class node
+class Node {
+
+//memberi akses modifier serta me-deklarasi variabel dan object node
+public :
+    string info;
+    Node* leftChild;
+    Node* rightChild;
+
+    //membuat constructor untuk class node
+    Node(string i, Node* l, Node* r) {
+        //memberi nilai parameter ke variabel
+        info = i;
+        leftChild = l;
+        rightChild = r;
+    }
+
+};
+

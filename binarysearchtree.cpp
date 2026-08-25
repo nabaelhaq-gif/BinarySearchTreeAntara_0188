c7092e760efe49c090d0cf6a7f13917352563264
nabaelhaq-gif <nabaelhaq@gmail.com>
2026-08-25 10:30:26 +0700

    memberi char ch den conditional statement dari input ch

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -136,6 +136,22 @@ int main()
         cout << "5. Exit" << endl;
         cout << "\nEnter your choice (1-5) : ";
 
+        char ch;
+        cin >> ch;
+        cout << endl;
+
+        //membuat conditional statement dari input ch
+        switch (ch)
+        {
+        case '1':
+        {
+            cout << "Enter a word : ";
+            string word;
+            cin >> word;
+            x.insert(word);
+            break;
+        }
         
+        }
     }
 }
